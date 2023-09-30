f8ddd36969416043d87853be87cbaac284759049
Beatrice Kerling Voight <Sloxet@gmail.com>
2023-09-30 18:00:38 +1300

    Updated sounds.

--- script.js
+++ script.js
@@ -1,53 +1,58 @@
 document.addEventListener('DOMContentLoaded', function() {
-  const logoContainer = document.querySelector('.under-construction-artwork-container');
-  const logo = document.getElementById('under-construction-artwork');
+    const logoContainer = document.querySelector('.under-construction-artwork-container');
+    const logo = document.getElementById('under-construction-artwork');
 
-  logoContainer.style.opacity = 1;
+    logoContainer.style.opacity = 1;
 
-  logo.addEventListener('click', playRandomSound);
+    logo.addEventListener('click', playRandomSound);
 
-  // Function to fade in the "Under Construction" text
-  const underConstruction = document.querySelector('.under-construction');
-  underConstruction.style.opacity = 0; // Initially set to 0
+    const underConstruction = document.querySelector('.under-construction');
+    underConstruction.style.opacity = 0;
 
-  function fadeInUnderConstructionText() {
-      const fadeInInterval = setInterval(() => {
-          underConstruction.style.opacity = parseFloat(underConstruction.style.opacity) + 0.05;
-          if (parseFloat(underConstruction.style.opacity) >= 1) {
-              clearInterval(fadeInInterval);
-          }
-      }, 50);
-  }
+    function fadeInUnderConstructionText() {
+        const fadeInInterval = setInterval(() => {
+            underConstruction.style.opacity = parseFloat(underConstruction.style.opacity) + 0.05;
+            if (parseFloat(underConstruction.style.opacity) >= 1) {
+                clearInterval(fadeInInterval);
+            }
+        }, 50);
+    }
 
-  fadeInUnderConstructionText();
+    fadeInUnderConstructionText();
 
-  // Fade in the social media icons one after the other
-  const socialIcons = document.querySelectorAll('.social-icon');
-  socialIcons.forEach((icon, index) => {
-      setTimeout(() => {
-          icon.style.opacity = 1;
-      }, 500 * index); // Adjust the delay here to control the fading speed
-  });
+    const socialIcons = document.querySelectorAll('.social-icon');
+    socialIcons.forEach((icon, index) => {
+        setTimeout(() => {
+            icon.style.opacity = 1;
+        }, 500 * index);
+    });
 
-  // Add animation for the social icons when hovered
-  socialIcons.forEach((icon) => {
-      icon.addEventListener('mouseover', () => {
-          icon.style.transform = 'scale(1.1)'; // Zoom effect
-      });
-      icon.addEventListener('mouseout', () => {
-          icon.style.transform = 'scale(1)'; // Restore original size
-      });
-  });
+    socialIcons.forEach((icon) => {
+        icon.addEventListener('mouseover', () => {
+            icon.style.transform = 'scale(1.1)';
+        });
+        icon.addEventListener('mouseout', () => {
+            icon.style.transform = 'scale(1)';
+        });
+    });
+
+    const sounds = [
+        'sound1.mp3',
+        'sound2.mp3',
+        'sound3.mp3',
+        'sound4.mp3',
+        'sound5.mp3',
+        'sound6.mp3',
+        'sound7.mp3',
+    ];
+    const audioElements = sounds.map(sound => {
+        const audio = new Audio('sounds/misc/' + sound);
+        audio.preload = 'auto';
+        return audio;
+    });
+
+    function playRandomSound() {
+        const randomIndex = Math.floor(Math.random() * audioElements.length);
+        audioElements[randomIndex].play();
+    }
 });
-
-function playRandomSound() {
-  const sounds = [
-      'sound1.mp3',
-      'sound2.mp3',
-      'sound3.mp3'
-  ];
-
-  const randomIndex = Math.floor(Math.random() * sounds.length);
-  const audio = new Audio('sounds/' + sounds[randomIndex]);
-  audio.play();
-}
